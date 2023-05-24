ce70d901a528211701d0706143e618e35ba8489b
Vinicius-Costa23 <vinicius.costa@sptech.school>
2023-05-24 11:56:48 -0300

    Subindo projeto

---
+++ site/src/models/usuarioModel.js
@@ -0,0 +1,22 @@
+var database = require("../database/config")
+
+function entrar(email, senha) {
+    var instrucao = `
+        SELECT * FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+function cadastrar(nome, email, senha) {
+    var instrucao = `
+        INSERT INTO usuario (nomeCompleto, email, senha) VALUES ('${nome}', '${email}', '${senha}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+module.exports = {
+    entrar,
+    cadastrar
+};
